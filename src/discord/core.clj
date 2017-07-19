0f56cfd1add805dcbce8ba10091145b7184f6826
Christopher Chapline <cachapline8@gmail.com>
2017-07-18 22:16:44 -0600

    Remove duplicate cog definition

--- src/discord/core.clj
+++ src/discord/core.clj
@@ -7,20 +7,6 @@
             [discord.http :as http])
   (:gen-class))
 
-;;; Example Cog implementation for admin commands
-(bot/defcog admin [client message]
-  (:kick
-    (doseq [user (:user-mentions message)]
-      (let [user-id (:id user)
-            guild-id (get-in message [:channel :guild-id] message)]
-        (http/kick client guild-id user-id))))
-
-  (:broadcast
-    (let [bcast-message (->> message :content utils/words rest (s/join " "))
-          servers (http/get-servers client)]
-      (doseq [server servers]
-        (http/send-message client server bcast-message)))))
-
 (defn -main
   "Creates a new discord bot and supplies a series of extensions to it."
   [& args]
