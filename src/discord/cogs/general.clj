4261f506f479b49b3b56f987e41c6be0e4d83b1a
Christopher Chapline <cachapline8@gmail.com>
2017-07-30 21:22:49 -0600

    Adds documentation for general cog commands

--- src/discord/cogs/general.clj
+++ src/discord/cogs/general.clj
@@ -6,16 +6,21 @@
             [clojure.string :as s]))
 
 (bot/defcommand say [client message]
+  "Tells the bot to echo back the content of your message."
   (bot/say (:content message)))
 
 (bot/defcommand botsay [client message]
+  "Tells the bot to echo back the content of your message and then deletes the user's original
+   message."
   (bot/say (:content message))
   (bot/delete message))
 
 (bot/defcommand working [client message]
+  "Posts the awesome Star Wars \"IT'S WORKING\" gif into the channel."
   (bot/say "https://giphy.com/gifs/9K2nFglCAQClO"))
 
 (bot/defcommand roll [client message]
+  "Rolls a dice between 1 and the user's supplied value (100 by default)."
   (if-let [potential-number (not-empty (:content message))]
     (try
       (let [limit (Integer/parseInt potential-number)]
@@ -25,11 +30,13 @@
     (bot/say (format ":game_die: %d :game_die:" (inc (rand-int 100))))))
 
 (bot/defcommand choose [client message]
+  "Tells the bot to choose one of the options supplied."
   (if-let [choices (utils/words (:content message))]
     (bot/say (rand-nth choices))
     (bot/say "Error: Nothing to choose from!")))
 
 (bot/defcommand servers [client message]
+  "Lists the servers that the bot is a part of."
   (let [bot-servers (http/get-servers client)]
     (->> client
          (http/get-servers)
@@ -40,10 +47,12 @@
 
 
 (bot/defcommand flip [client message]
+  "Tells the bot to flip a coin."
   (bot/say (format "Flipping a coin....%s" (rand-nth ["Heads" "Tails"]))))
 
 
 (bot/defcommand rps [client message]
+  "Plays Rock, Paper, Scissors with the bot"
   (if-let [user-choice (-> message :content utils/words first keyword)]
     (if (some #{user-choice} [:rock :paper :scissors])
       (let [bot-choice (rand-nth [:rock :paper :scissors])]
