0a01cc66dde3a950220866babedd2324cfa89585
Christopher Chapline <cachapline8@gmail.com>
2018-05-15 20:37:15 -0600

    Can optionally supply default when loading configs

--- src/discord/config.clj
+++ src/discord/config.clj
@@ -9,18 +9,17 @@
 ;;; Saving, loading, and checking config files
 (defmulti file-io
   "Manages the saving and loading of file-io files"
-  (fn [filename operation & {:keys [data]}]
+  (fn [filename operation & {:keys [data default]}]
     operation))
 
 (defmethod file-io :save [filename _ & {:keys [data]}]
   (spit filename (json/write-str data)))
 
-(defmethod file-io :load [filename _]
+(defmethod file-io :load [filename _ & {:keys [default]}]
   (try
     (json/read-str (slurp filename) :key-fn keyword)
     (catch IOException ioe
-      (log/error ioe)
-      [])))
+      (or default []))))
 
 (defmethod file-io :check [filename _]
   (let [f (io/as-file filename)]
