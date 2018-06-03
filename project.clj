3a0c1e4addce304517a5d1906a85b3f6c33540fb
Christopher Chapline <cachapline8@gmail.com>
2018-06-02 23:35:50 -0600

    Version 1.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject discord.clj "1.0.0"
+(defproject discord.clj "1.1.0"
   :description "A library for creating Discord bots in Clojure."
   :url "https://github.com/gizmo385/discord.clj"
   :license {:name "Eclipse Public License"
