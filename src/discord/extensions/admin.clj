ad8f5101fc073b40b7cd6ea505be2192524f0e3b
Christopher Chapline <cachapline8@gmail.com>
2019-04-20 00:20:52 -0600

    Fix admin subcommand documentation

--- src/discord/extensions/admin.clj
+++ src/discord/extensions/admin.clj
@@ -11,30 +11,30 @@
 (bot/defextension admin [client message]
   "Commands for common server adminstrative tasks."
   (:kick
-    {:requires [perm/KICK-MEMBERS]}
     "Kicks the mentioned users from the server."
+    {:requires [perm/KICK-MEMBERS]}
     (doseq [user (:user-mentions message)]
       (let [user-id (:id user)
             guild-id (get-in message [:channel :guild-id])]
         (http/kick client guild-id user-id))))
 
   (:ban
-    {:requires [perm/BAN-MEMBERS]}
     "Bans the mentioned users from the server."
+    {:requires [perm/BAN-MEMBERS]}
     (doseq [user (:user-mentions message)]
       (let [guild-id (get-in message [:channel :guild-id])]
         (http/ban client guild-id user))))
 
   (:unban
-    {:requires [perm/BAN-MEMBERS]}
     "Unbans the mentioned users from the server."
+    {:requires [perm/BAN-MEMBERS]}
     (doseq [user (:user-mentions message)]
       (let [guild-id (get-in message [:channel :guild-id])]
         (http/unban client guild-id user))))
 
   (:broadcast
-    {:requires [perm/ADMINISTRATOR]}
     "Sends a message to all servers to which the bot is connected."
+    {:requires [perm/ADMINISTRATOR]}
     (let [bcast-message (->> message :content utils/words rest (s/join " "))
           servers (http/get-servers client)]
       (doseq [server servers]
@@ -64,7 +64,7 @@
         (bot/say (format "The region \"%s\" does not exist.")))))
 
   (:shutdown
-    {:requires [perm/ADMINISTRATOR]}
     "Attempts to cleanly shutdown the bot."
+    {:requires [perm/ADMINISTRATOR]}
     (.close client)
     (System/exit 0)))
