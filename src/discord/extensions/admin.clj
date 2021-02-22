7ae555a90200bba7ef759d1841beceace78f8cba
Christopher Chapline <gizmo385@Christopher-PC.localdomain>
2021-02-21 22:29:11 -0700

    Rewrite admin module in new style

--- src/discord/extensions/admin.clj
+++ src/discord/extensions/admin.clj
@@ -8,62 +8,50 @@
             [discord.http :as http]
             [discord.types :as types]))
 
-(bot/defextension admin [client message]
-  "Commands for common server adminstrative tasks."
-  (:kick
-    "Kicks the mentioned users from the server."
-    {:requires [perm/KICK-MEMBERS]}
-    (doseq [user (:user-mentions message)]
-      (let [user-id (:id user)
-            guild-id (get-in message [:channel :guild-id])]
-        (http/kick client guild-id user-id))))
 
-  (:ban
-    "Bans the mentioned users from the server."
-    {:requires [perm/BAN-MEMBERS]}
-    (doseq [user (:user-mentions message)]
-      (let [guild-id (get-in message [:channel :guild-id])]
-        (http/ban client guild-id user))))
-
-  (:unban
-    "Unbans the mentioned users from the server."
-    {:requires [perm/BAN-MEMBERS]}
-    (doseq [user (:user-mentions message)]
-      (let [guild-id (get-in message [:channel :guild-id])]
-        (http/unban client guild-id user))))
-
-  (:broadcast
-    "Sends a message to all servers to which the bot is connected."
-    {:requires [perm/ADMINISTRATOR]}
-    (let [bcast-message (->> message :content utils/words rest (s/join " "))
-          servers (http/get-servers client)]
-      (doseq [server servers]
-        (http/send-message client server bcast-message))))
-
-  (:voiceregion
-    "Returns the current voice region for the guild."
-    (let [guild-id      (get-in message [:channel :guild-id])
-          guild         (http/get-guild client guild-id)
-          voice-region  (-> guild :region name)]
-      (bot/say (format "The guild's voice region is currently \"%s\"" voice-region))))
-
-  (:regionlist
-    "Lists all supported voice regions."
-    (bot/say (format "Supported voice regions: %s" (s/join ", " (keys types/server-region)))))
-
-  (:regionmove
-    "Moves the voice region for the guild to a new location."
-    {:requires [perm/MANAGE-GUILD]}
-    (let [desired-region (->> message :content utils/words rest (s/join " "))
+(defn doto-mentioned-users
+  [client message action]
+  (doseq [user (:user-mentions message)]
+    (let [user-id (:id user)
           guild-id (get-in message [:channel :guild-id])]
-      (if-let [region-keyword (get types/server-region desired-region)]
-        (do
-          (bot/say (format "Moving voice server to \"%s\"" desired-region))
-          (http/modify-server client guild-id :region desired-region))
-        (bot/say (format "The region \"%s\" does not exist.")))))
+      (action client guild-id user-id))))
 
-  (:shutdown
-    "Attempts to cleanly shutdown the bot."
-    {:requires [perm/ADMINISTRATOR]}
-    (.close client)
-    (System/exit 0)))
+(bot/install-modules!
+  (bot/with-module :admin
+    (bot/with-module :player
+      (bot/command :kick [client message & _] (doto-mentioned-users client message http/kick))
+      (bot/command :ban [client message & _] (doto-mentioned-users client message http/ban))
+      (bot/command :unban [client message & _] (doto-mentioned-users client message http/unban)))
+
+    (bot/with-module :region
+      (bot/command
+        :get [client message]
+        (let [guild-id      (get-in message [:channel :guild-id])
+              guild         (http/get-guild client guild-id)
+              voice-region  (-> guild :region name)]
+          (bot/reply-in-channel
+            client message (format "The guild's voice region is currently \"%s\"" voice-region))))
+      (bot/command
+        :list [client message]
+        (let [reply (format "Supported voice regions: %s" (s/join ", " (keys types/server-region)))]
+          (bot/reply-in-channel client message reply)))
+      (bot/command
+        :move [client message new-region]
+        (let [guild-id (get-in message [:channel :guild-id])]
+          (if-let [region-keyword (get types/server-region new-region)]
+            (do
+              (bot/reply-in-channel
+                client message (format "Moving voice server to \"%s\"" new-region))
+              (http/modify-server client guild-id :region new-region))
+            (bot/reply-in-channel client message (format "The region \"%s\" does not exist."))))))
+
+    (bot/command
+      :shutdown [client message]
+      (.close client)
+      (System/exit 0))
+    (bot/command
+      :broadcast [client message message-to-broadcast]
+      (let [bcast-message (->> message :content utils/words rest (s/join " "))
+            servers (http/get-servers client)]
+        (doseq [server servers]
+          (http/send-message client server bcast-message))))))
