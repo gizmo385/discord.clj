5a516ce2b21cc8d8805388b0819491b3a8d54234
Christopher Chapline <cachapline8@gmail.com>
2021-03-02 00:35:17 -0700

    Rewrite the alias extension using new style

--- src/discord/extensions/alias.clj
+++ src/discord/extensions/alias.clj
@@ -1,102 +1,86 @@
 (ns discord.extensions.alias
   (:require [clojure.core.async :refer [go >!] :as async]
             [clojure.string :refer [starts-with?] :as s]
+            [taoensso.timbre :as timbre]
             [discord.bot :as bot]
             [discord.embeds :as embeds]
             [discord.permissions :as perms]
             [discord.config :as config]
-            [discord.constants :as const]
-            [discord.utils :as utils]))
+            [discord.constants :as const]))
 
 ;;; Create the configuraton file if necessary
 (defonce alias-settings "data/alias/aliases.json")
 (config/file-io alias-settings :check)
 
-;;; We're going to need some kind of global so that we don't have to keep hitting the file system
-;;; when attempting to parse an alias
-(defonce alias-cache (atom {}))
-(defonce last-loaded-aliases (atom nil))
+;;; We'll define an atom to maintain the map of aliases that are defined within the system.
+(defonce alias->command (atom {}))
 
 ;;; Some helper functions for interacting with the alias definitions
-(defn alias-cache-fresh?
-  "This function determines whether or not the alias cache is 'fresh'. For the purposes of this
-   application, we define a 'fresh' cache as a cache that has been updated in the last 5 minutes."
-  []
-  (let [last-loaded @last-loaded-aliases
-        now (System/currentTimeMillis)]
-    (and (some? last-loaded)
-         (< (- now last-loaded) (* 5 const/MILLISECONDS-IN-MINUTE)))))
+(defn- add-alias! [new-alias command]
+  (swap! alias->command assoc new-alias command)
+  (config/file-io alias-settings :save :data @alias->command))
 
-(defn- get-aliases []
-  (if (alias-cache-fresh?)
-    @alias-cache
-    (let [aliases (config/file-io alias-settings :load)]
-      (reset! alias-cache aliases)
-      (reset! last-loaded-aliases (System/currentTimeMillis))
-      aliases)))
+(defn- remove-aliases! [aliases-to-remove]
+    (swap! alias->command #(apply dissoc % aliases-to-remove))
+    (config/file-io alias-settings :save :data @alias->command))
 
-(defn- add-alias [new-alias command]
-  (let [old-aliases (get-aliases)
-        new-aliases (assoc old-aliases new-alias command)]
-    (config/file-io alias-settings :save :data new-aliases)
-    (reset! alias-cache new-aliases)
-    (reset! last-loaded-aliases (System/currentTimeMillis))))
+(defn- clear-aliases! []
+  (reset! alias->command {})
+  (config/file-io alias-settings :save :data @alias->command))
 
-(defn- remove-aliases [aliases-to-remove]
-  (let [old-aliases (get-aliases)
-        new-aliases (apply dissoc old-aliases aliases-to-remove)]
-    (config/file-io alias-settings :save :data new-aliases)
-    (reset! alias-cache new-aliases)
-    (reset! last-loaded-aliases (System/currentTimeMillis))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Command definitions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn list-aliases
+  [client message]
+  (if-let [aliases (not-empty @alias->command)]
+    (->> aliases
+         (reduce
+           (fn [embed [defined-alias command]]
+             (embeds/+field embed (name defined-alias) command))
+           (embeds/create-embed :title "Available aliases:"))
+         (bot/reply-in-channel client message))
+    (bot/reply-in-channel client message "No aliases defined.")))
 
-(defn- clear-aliases []
-  (let [new-aliases {}]
-    (config/file-io alias-settings :save :data new-aliases)
-    (reset! alias-cache new-aliases)
-    (reset! last-loaded-aliases (System/currentTimeMillis))))
-
-;;; The 'alias' extension will handle the addition and removal of aliases into the system.
-(bot/defextension alias [client message]
-  "Adding aliases for different bot commands."
-  (:list
-    "Lists the currently available aliases."
-    (if-let [aliases (not-empty (get-aliases))]
-      (loop [aliases aliases
-             embed (embeds/create-embed :title "Available aliases:")]
-        (if-let [[defined-alias command] (first aliases)]
-          (recur (rest aliases)
-                 (embeds/+field embed (name defined-alias) command))
-          (bot/say embed)))
-      (bot/say "Could not find any defined aliases. Try adding one with 'alias add'!")))
-
-  (:add
-    "Adds a new alias to the system."
-    {:requires [perms/MANAGE-MESSAGES]}
-    (if-let [[commmand new-alias & command] (utils/words (:content message))]
+(defn add-alias
+  [client message new-alias & command]
+  (if (perms/has-permission? client message perms/ADMINISTRATOR)
+    (if (not-empty command)
       (do
-        (add-alias new-alias (s/join " " command))
-        (bot/say "Sucessfully added new alias!"))
-      (bot/say "Error! Please supply a valid alias definition: alias add <alias> <command words>")))
+        (add-alias! new-alias (s/join " " command))
+        (bot/reply-in-channel client message (format "Added new alias '%s'." new-alias)))
+      (bot/reply-in-channel client message "Error: Please supply a valid alias definition."))
+    (bot/reply-in-channel "You don't have permission to add new aliases.")))
 
-  (:remove
-    "Removes the specified aliases"
-    {:requires [perms/MANAGE-MESSAGES]}
-    (let [aliases-to-remove (utils/words (:content message))]
-      (remove-aliases aliases-to-remove)
-      (bot/say (format "Sucessfully removed: %s" (s/join ", " aliases-to-remove)))))
+(defn remove-aliases
+  [client message & aliases-to-remove]
+  (if (perms/has-permission? client message perms/ADMINISTRATOR)
+    (do (remove-aliases! aliases-to-remove)
+        (bot/reply-in-channel client message "Removed aliases."))
+    (bot/reply-in-channel client message "You don't have permission to remove aliases.")))
 
-  (:clear
-    "Removes all installed aliases."
-    {:requires [perms/MANAGE-MESSAGES]}
-    (clear-aliases)
-    (bot/say "I've removed all installed aliases.")))
+(defn clear-all-aliases
+  [client message]
+  (if (perms/has-permission? client message perms/ADMINISTRATOR)
+    (do (clear-aliases!)
+        (bot/reply-in-channel client message "Removed all aliases."))
+    (bot/reply-in-channel client message "You don't have permission to remove aliases.")))
 
-;;; Now we need to build a handler to intercept messages received and look for aliases and send the
-;;; mapped command back to the client's receive channel.
-(bot/defhandler alias-message-handler [prefix client message]
-  (let [aliases         (get-aliases)
-        receive-chan    (:receive-channel client)
+(bot/install-modules!
+  (bot/with-module :alias
+    {:add add-alias
+     :list list-aliases
+     :remove remove-aliases
+     :clear clear-all-aliases}))
+
+(defn alias-message-handler
+  [prefix client message]
+  (let [receive-chan    (:receive-channel client)
         message-content (:content message)]
-    (doseq [[defined-alias command] aliases]
-      (if (= message-content (str prefix (name defined-alias)))
+    (doseq [[defined-alias command] @alias->command]
+      (timbre/infof "Checking message '%s' against '%s'" message-content defined-alias)
+      (when (= message-content (name defined-alias))
+        (timbre/infof "Alias matches: %s" defined-alias)
         (go (>! receive-chan (assoc message :content command)))))))
+
+(bot/add-handler! alias-message-handler)
