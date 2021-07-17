a380f6f708113c8f6f2319bcb788464eee324db8
Christopher Chapline <cachapline8@gmail.com>
2021-07-17 15:37:59 -0600

    Minor updates

--- src/discord/core.clj
+++ src/discord/core.clj
@@ -1,12 +1,10 @@
 (ns discord.core
   (:require
-    [discord.config :as config]
-    [discord.types.auth :as auth]
+    ;; We import this to ensure the ig/init defmethod is evaluated.
     [discord.bot :as bot]
     [integrant.core :as ig])
   (:gen-class))
 
-
 (defn -main
   [& args]
-  (ig/init (ig/read-string (slurp "resources/config.edn"))))
+  (-> "resources/config.edn" slurp ig/read-string ig/init))
