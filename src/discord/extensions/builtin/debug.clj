765a4bb66b9dfb82544204e78967887c62dc90d6
Christopher Chapline <cachapline8@gmail.com>
2021-07-19 23:38:57 -0600

    Debug extensions + account-creation-date

--- src/discord/extensions/builtin/debug.clj
+++ src/discord/extensions/builtin/debug.clj
@@ -1,6 +1,5 @@
 (ns discord.extensions.builtin.debug
   (:require
-    [clojure.pprint :refer [pprint]]
     [discord.api.guilds :as guilds-api]
     [discord.interactions.core :as i]
     [discord.interactions.slash :as slash]
@@ -8,16 +7,21 @@
     [discord.extensions.utils :as ext-utils]
     [discord.types.permissions :as perms]
     [discord.types.role :as roles]
-    [discord.embeds :as e]
-    ))
+    [discord.types.snowflake :as sf]
+    [discord.embeds :as e]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Slash command definitions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (slash/register-globally-on-startup!
   (slash/command
     :debug "Debugging information"
-    (slash/sub-command :get-my-permissions "Retrieve your permissions")
     (slash/sub-command
-      :get-permissions "Retrieve a different user's permissions"
-      (slash/user-option :user "The user whose permissions you want to check." :required true))))
+      :get-permissions "Retrieve permissions for yourself or the mentioned user."
+      (slash/user-option :user "The user whose permissions you want to check."))
+    (slash/sub-command
+      :account-creation-date "Determine the account creation date for yourself or the mentioend user."
+      (slash/user-option :user "The user whose account creation date you want to check."))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Debugging user permissions
@@ -33,6 +37,8 @@
        (apply merge-with #(or %1 %2))))
 
 (defn permission-map->embed
+  "Given a map of permissions to a boolean indicating whether or not a user has that permission,
+   generates an embed that displays that information."
   [permission-map]
   (reduce (fn [embed [permission possessed?]]
             (e/+field embed (name permission) (str possessed?) :inline true))
@@ -40,7 +46,9 @@
           permission-map))
 
 (defn build-user-permissions-embed
-  [user-id user-role-ids guild-roles]
+  "Given the IDs of the roles that they have and the roles available in the guild generates an embed
+   showing the permissions that user does and does not have."
+  [user-role-ids guild-roles]
   (let [user-roles (filter (fn [role] (some #{(:id role)} user-role-ids)) guild-roles)
         user-permissions (determine-user-permissions user-roles guild-roles)]
     (permission-map->embed user-permissions)))
@@ -48,24 +56,22 @@
 (defmethod slash/handle-slash-command-interaction [:debug :get-permissions]
   [{:keys [interaction arguments]} auth metadata]
   (if-let [guild-id (:guild-id interaction)]
-    (let [user-id (:user arguments)
-          _ (println :user-id user-id)
-          guild-member (guilds-api/get-guild-member auth guild-id user-id)
+    (let [user-id (or (:user arguments) (get-in interaction [:member :user :id]))
+          guild-member (if (:user arguments)
+                         (guilds-api/get-guild-member auth guild-id user-id)
+                         (:member interaction))
           user-role-ids (:role-ids guild-member)
           guild-roles (guilds-api/get-guild-roles auth (:guild-id interaction))
-          permissions-embed (build-user-permissions-embed user-id user-role-ids guild-roles)
+          permissions-embed (build-user-permissions-embed user-role-ids guild-roles)
           message (format "Permissions for <@%s>:" user-id)]
-      (println :message message)
-      (println :permissions-embed permissions-embed)
       (i/channel-message-response interaction auth message nil [permissions-embed]))
     (i/channel-message-response interaction auth "Please run this command within a guild!" nil nil)))
 
-(defmethod slash/handle-slash-command-interaction [:debug :get-my-permissions]
-  [{:keys [interaction]} auth metadata]
-  (if-let [user-role-ids (get-in interaction [:member :role-ids])]
-    (let [user-id (get-in interaction [:member :user :id])
-          guild-roles (guilds-api/get-guild-roles auth (:guild-id interaction))
-          permissions-embed (build-user-permissions-embed user-id user-role-ids guild-roles)
-          message (format "Permissions for <@%s>:" user-id)]
-      (i/channel-message-response interaction auth message nil [permissions-embed]))
-    (i/channel-message-response interaction auth "Please run this command within a guild!" nil nil)))
+(defmethod slash/handle-slash-command-interaction [:debug :account-creation-date]
+  [{:keys [interaction arguments]} auth _]
+  (let [user-id (or (:user arguments)
+                    (get-in interaction [:member :user :id])
+                    (get-in interaction [:user :id]))
+        creation-timestamp (quot (sf/snowflake->timestamp user-id) 1000)
+        message (format "Account for <@%s> created at <t:%s:F>" user-id creation-timestamp)]
+    (i/channel-message-response interaction auth message nil nil)))
